d43e8817d472121be5ef4f9b59dabfb2443b6042
김기택 <richtack@gimgitaeg-ui-MacBookPro.local>
2020-04-13 21:05:51 +0900

    Apply Material Ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer';
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell'; 
+import { withStyles } from '@material-ui/core/styles';
+
 // import Clock from './Example/clock';
 // import ApiExample from './Example/ApiExample';
 // import EventHandling from './Example/EventHandling';
 // import Customer from './components/Customer';
-
+const styles = theme => ({
+  root:{
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"  
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 const customers = [
 {
   'id': 1,
@@ -33,9 +50,22 @@ const customers = [
 ]
 class App extends Component{
   render(){
+     const { classes } = this.props;
   return (
-    <div>
-      {
+    <Paper className={classes.root}>
+     <Table className={classes.table}>
+       <TableHead>
+         <TableRow>
+           <TableCell>번호</TableCell>
+           <TableCell>이미지</TableCell>
+           <TableCell>이름</TableCell>
+           <TableCell>생년월일</TableCell>
+           <TableCell>성별</TableCell>
+           <TableCell>직업</TableCell>
+         </TableRow>
+       </TableHead>
+       <TableBody>
+       {
         // customers.map(c =>{})  한 줄로 사용할수 았음
         customers.map(c => {  // 새로운 배열을 합쳐서 만들어준다
           return (
@@ -51,9 +81,12 @@ class App extends Component{
           )
         })
       }
-    </div>
+       </TableBody>
+     </Table>
+      
+    </Paper>
    
   ); 
 }
 }
-export default App;
+export default withStyles(styles)(App);
